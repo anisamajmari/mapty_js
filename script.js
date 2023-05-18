b6dbd911491f8c617d323998050437c2f12373d9
Anisa Majmari <anisamajmari@gmail.com>
2023-05-18 12:16:27 +0200

    get position, load map

--- script.js
+++ script.js
@@ -21,3 +21,36 @@ class Running extends Workout {
     this.cadence = cadence;
   }
 }
+
+class App {
+  #map;
+  #zoomLevel = 12;
+  constructor() {
+    this._getLocation();
+  }
+
+  _getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Cloud not get your position");
+        }
+      );
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+    this.#map = L.map("map").setView(coords, this.#zoomLevel);
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+  }
+}
+
+const app = new App();
